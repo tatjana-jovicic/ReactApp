dccbcab2766f9a18a4201b92cf5371460de517eb
unknown <tatjanajovicic19@gmail.com>
2024-03-27 18:51:39 +0100

    update text in Home Page

--- src/features/Home/Home.jsx
+++ src/features/Home/Home.jsx
@@ -1,12 +1,16 @@
-
 const Home = () => {
-    return (
-        <>
-            <h1>Home</h1>
-            <p>Welcome to our site!</p>
-            <p>We are here to help in any way we can!</p>
-        </>
-    )
-}
+  return (
+    <>
+      <h1>Home</h1>
+      <p>Welcome to our site!</p>
+      <p>We are here to help in any way we can!</p>
+      <p>We are here to help in any way we can!</p>
+      <p>We are here to help in any way we can!</p>
+      <p>We are here to help in any way we can!</p>
+      <p>We are here to help in any way we can!</p>
+      <p>We are here to help in any way we can!</p>
+    </>
+  );
+};
 
-export default Home
+export default Home;
